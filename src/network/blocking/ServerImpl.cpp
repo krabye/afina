cf115e37877165116a87ccbaf444304215c5176c
sir_krabye <v.efimov@corp.mail.ru>
2017-10-18 23:03:15 +0300

    hw4

--- src/network/blocking/ServerImpl.cpp
+++ src/network/blocking/ServerImpl.cpp
@@ -52,48 +52,53 @@ void ServerImpl::Start(uint32_t port, uint16_t n_workers) {
     sigset_t sig_mask;
     sigemptyset(&sig_mask);
     sigaddset(&sig_mask, SIGPIPE);
-    if (pthread_sigmask(SIG_BLOCK, &sig_mask, NULL) != 0) {
-        throw std::runtime_error("Unable to mask SIGPIPE");
-    }
-
-    // Setup server parameters BEFORE thread created, that will guarantee
-    // variable value visibility
-    max_workers = n_workers;
-    listen_port = port;
-
-    // The pthread_create function creates a new thread.
-    //
-    // The first parameter is a pointer to a pthread_t variable, which we can use
-    // in the remainder of the program to manage this thread.
-    //
-    // The second parameter is used to specify the attributes of this new thread
-    // (e.g., its stack size). We can leave it NULL here.
-    //
-    // The third parameter is the function this thread will run. This function *must*
-    // have the following prototype:
-    //    void *f(void *args);
-    //
-    // Note how the function expects a single parameter of type void*. We are using it to
-    // pass this pointer in order to proxy call to the class member function. The fourth
-    // parameter to pthread_create is used to specify this parameter value.
-    //
-    // The thread we are creating here is the "server thread", which will be
-    // responsible for listening on port 23300 for incoming connections. This thread,
-    // in turn, will spawn threads to service each incoming connection, allowing
-    // multiple clients to connect simultaneously.
-    // Note that, in this particular example, creating a "server thread" is redundant,
-    // since there will only be one server thread, and the program's main thread (the
-    // one running main()) could fulfill this purpose.
-    running.store(true);
-    if (pthread_create(&accept_thread, NULL, ServerImpl::RunAcceptorProxy, this) < 0) {
-        throw std::runtime_error("Could not create server thread");
-    }
+    try {
+    	if (pthread_sigmask(SIG_BLOCK, &sig_mask, NULL) != 0) {
+            throw std::runtime_error("Unable to mask SIGPIPE");
+        }
+    
+        // Setup server parameters BEFORE thread created, that will guarantee
+        // variable value visibility
+        max_workers = n_workers;
+        listen_port = port;
+    
+        // The pthread_create function creates a new thread.
+        //
+        // The first parameter is a pointer to a pthread_t variable, which we can use
+        // in the remainder of the program to manage this thread.
+        //
+        // The second parameter is used to specify the attributes of this new thread
+        // (e.g., its stack size). We can leave it NULL here.
+        //
+        // The third parameter is the function this thread will run. This function *must*
+        // have the following prototype:
+        //    void *f(void *args);
+        //
+        // Note how the function expects a single parameter of type void*. We are using it to
+        // pass this pointer in order to proxy call to the class member function. The fourth
+        // parameter to pthread_create is used to specify this parameter value.
+        //
+        // The thread we are creating here is the "server thread", which will be
+        // responsible for listening on port 23300 for incoming connections. This thread,
+        // in turn, will spawn threads to service each incoming connection, allowing
+        // multiple clients to connect simultaneously.
+        // Note that, in this particular example, creating a "server thread" is redundant,
+        // since there will only be one server thread, and the program's main thread (the
+        // one running main()) could fulfill this purpose.
+        running.store(true);
+        if (pthread_create(&accept_thread, NULL, ServerImpl::RunAcceptorProxy, this) < 0) {
+            throw std::runtime_error("Could not create server thread");
+        } 
+    } catch (std::runtime_error &ex) {
+		std::cerr << "Server fails: " << ex.what() << std::endl;
+	}
 }
 
 // See Server.h
 void ServerImpl::Stop() {
     std::cout << "network debug: " << __PRETTY_FUNCTION__ << std::endl;
     running.store(false);
+    shutdown(server_socket, SHUT_RDWR);
 }
 
 // See Server.h
@@ -122,76 +127,86 @@ void ServerImpl::RunAcceptor() {
     server_addr.sin_port = htons(listen_port); // TCP port number
     server_addr.sin_addr.s_addr = INADDR_ANY;  // Bind to any address
 
-    // Arguments are:
-    // - Family: IPv4
-    // - Type: Full-duplex stream (reliable)
-    // - Protocol: TCP
-    int server_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (server_socket == -1) {
-        throw std::runtime_error("Failed to open socket");
-    }
+    try {
+	    // Arguments are:
+	    // - Family: IPv4
+	    // - Type: Full-duplex stream (reliable)
+	    // - Protocol: TCP
+	    server_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	    if (server_socket == -1) {
+	        throw std::runtime_error("Failed to open socket");
+	    }
 
-    // when the server closes the socket,the connection must stay in the TIME_WAIT state to
-    // make sure the client received the acknowledgement that the connection has been terminated.
-    // During this time, this port is unavailable to other processes, unless we specify this option
-    //
-    // This option let kernel knows that we are OK that multiple threads/processes are listen on the
-    // same port. In a such case kernel will balance input traffic between all listeners (except those who
-    // are closed already)
-    int opts = 1;
-    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opts, sizeof(opts)) == -1) {
-        close(server_socket);
-        throw std::runtime_error("Socket setsockopt() failed");
-    }
+	    // when the server closes the socket,the connection must stay in the TIME_WAIT state to
+	    // make sure the client received the acknowledgement that the connection has been terminated.
+	    // During this time, this port is unavailable to other processes, unless we specify this option
+	    //
+	    // This option let kernel knows that we are OK that multiple threads/processes are listen on the
+	    // same port. In a such case kernel will balance input traffic between all listeners (except those who
+	    // are closed already)
+	    int opts = 1;
+	    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opts, sizeof(opts)) == -1) {
+	        close(server_socket);
+	        throw std::runtime_error("Socket setsockopt() failed");
+	    }
 
-    // Bind the socket to the address. In other words let kernel know data for what address we'd
-    // like to see in the socket
-    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        close(server_socket);
-        throw std::runtime_error("Socket bind() failed");
-    }
+	    // Bind the socket to the address. In other words let kernel know data for what address we'd
+	    // like to see in the socket
+	    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+	        close(server_socket);
+	        throw std::runtime_error("Socket bind() failed");
+	    }
 
-    // Start listening. The second parameter is the "backlog", or the maximum number of
-    // connections that we'll allow to queue up. Note that listen() doesn't block until
-    // incoming connections arrive. It just makesthe OS aware that this process is willing
-    // to accept connections on this socket (which is bound to a specific IP and port)
-    if (listen(server_socket, 5) == -1) {
-        close(server_socket);
-        throw std::runtime_error("Socket listen() failed");
-    }
+	    // Start listening. The second parameter is the "backlog", or the maximum number of
+	    // connections that we'll allow to queue up. Note that listen() doesn't block until
+	    // incoming connections arrive. It just makesthe OS aware that this process is willing
+	    // to accept connections on this socket (which is bound to a specific IP and port)
+	    if (listen(server_socket, 5) == -1) {
+	        close(server_socket);
+	        throw std::runtime_error("Socket listen() failed");
+	    }
 
-    int client_socket;
-    struct sockaddr_in client_addr;
-    socklen_t sinSize = sizeof(struct sockaddr_in);
-    while (running.load()) {
-        std::cout << "network debug: waiting for connection..." << std::endl;
+	    int client_socket;
+	    struct sockaddr_in client_addr;
+	    socklen_t sinSize = sizeof(struct sockaddr_in);
+	    while (running.load()) {
+	        std::cout << "network debug: waiting for connection..." << std::endl;
 
-        // When an incoming connection arrives, accept it. The call to accept() blocks until
-        // the incoming connection arrives
-        if ((client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &sinSize)) == -1) {
-            close(server_socket);
-            throw std::runtime_error("Socket accept() failed");
-        }
+	        // When an incoming connection arrives, accept it. The call to accept() blocks until
+	        // the incoming connection arrives
+	        if ((client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &sinSize)) == -1) {
+	            close(server_socket);
+	            throw std::runtime_error("Socket accept() failed");
+	        }
 
-        con_mutex.lock();
-        if (connections.size() >= max_workers) {
-            con_mutex.unlock();
-            std::cerr << "not enough workers\n";
-            close(client_socket);
-        } else {
-            con_mutex.unlock();
-            pthread_t cl_connection;
-            if (pthread_detach(cl_connection) < 0) {
-                throw std::runtime_error("Could not detach client thread");
-            }
-            connections.push_back(cl_connection);
-            Data data = {this, client_socket};
-            if (pthread_create(&connections.back(), NULL, ServerImpl::RunConnectionProxy, &data) < 0) {
-                throw std::runtime_error("Could not create client thread");
-            }
-        }
-        
-    }
+	        con_mutex.lock();
+	        if (connections.size() >= max_workers) {
+	            con_mutex.unlock();
+	            std::cerr << "not enough workers\n";
+	            close(client_socket);
+	        } else {
+	            con_mutex.unlock();
+	            pthread_t cl_connection;
+	            pthread_attr_t attr;
+	            if (pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED) < 0) {
+	            	throw std::runtime_error("Could not set detache attribute");
+	            }
+	            
+	            connections.push_back(cl_connection);
+	            Data data = {this, client_socket};
+	            if (pthread_create(&connections.back(), &attr, ServerImpl::RunConnectionProxy, &data) < 0) {
+	                throw std::runtime_error("Could not create client thread");
+	            }
+
+	            // if (pthread_detach(cl_connection) < 0) {
+	            //     throw std::runtime_error("Could not detach client thread");
+	            // }
+	        }
+	        
+	    }
+	} catch (std::runtime_error &ex) {
+		std::cerr << "Server fails: " << ex.what() << std::endl;
+	}
 
     //Waiting until all threads stop working
     while (true) {
@@ -276,6 +291,7 @@ void ServerImpl::RunConnection(int client_socket) {
         }
     } catch (...) {
         close(client_socket);
+        std::cout << "smth wrong\n";
         return;
     }
     close(client_socket);
