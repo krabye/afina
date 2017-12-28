336aa4bd0e1e92d4c2a61cef74a90bdf8376f631
Parallels <vladefimovv@ya.ru>
2017-12-28 17:36:56 +0300

    hw10 fix

--- src/coroutine/Engine.cpp
+++ src/coroutine/Engine.cpp
@@ -9,21 +9,21 @@ namespace Coroutine {
 
 void Engine::Store(context &ctx) {
 	volatile char stack_end;
-	if (std::get<0>(ctx->Stack) != nullptr)
+	if (std::get<0>(ctx.Stack) != nullptr)
 		delete[] std::get<0>(ctx.Stack);
 	uint32_t len = StackBottom - &stack_end;
 	ctx.Stack = std::make_tuple(new char[len], len);
-	std::copy(&stack_end, StackBottom, std::get<0>(ctx.Stack));
+	memcpy(std::get<0>(ctx.Stack), (const void*)&stack_end, len);
 }
 
 void Engine::Restore(context &ctx) {
 	volatile char stack_end;
-	if (&stack_end > std::get<0>(ctx.Stack))
+	if (&stack_end > StackBottom - std::get<1>(ctx.Stack))
 		Restore(ctx);
 
-	std::copy(std::get<0>(ctx.Stack), 
-		std::get<0>(ctx.Stack)+std::get<1>(ctx.Stack), 
-		StackBottom - std::get<1>(ctx.Stack));
+	memcpy(StackBottom - std::get<1>(ctx.Stack),
+		std::get<0>(ctx.Stack), 
+		std::get<1>(ctx.Stack));
 
 	longjmp(ctx.Environment, 1);
 }
